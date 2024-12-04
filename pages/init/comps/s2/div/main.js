0c200e6b570747d6954c99f3f910fc7aa79b8e85
philippe <philippe.idalgoprestes@gmail.com>
2024-12-04 17:54:07 -0300

    hun

--- pages/init/comps/s2/div/main.js
+++ pages/init/comps/s2/div/main.js
@@ -3,11 +3,11 @@ import text from "./text/main.js"
 export default function div(){
     let style = `
         {
-            padding: 5px 10px;
             border-radius: 5px;
             background: white;
             box-shadow: 0px 0px 3px 0px black;
             width: fit-content;
+            padding: 5px 10px;
             cursor: pointer;
             transition: box-shadow var(--transitionTime);
         }
